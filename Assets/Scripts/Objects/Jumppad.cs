b8e8aa7f825ec2f4c55e1a3eea72cb5403af001b
hayoungbin <zinks1998@gmail.com>
2024-05-30 21:10:06 +0900

    사용하지 않는 Updata 제거

--- Assets/Scripts/Objects/Jumppad.cs
+++ Assets/Scripts/Objects/Jumppad.cs
@@ -4,10 +4,6 @@ using UnityEngine;
 
 public class Jumppad : MonoBehaviour
 {
-    private void Update()
-    {
-        
-    }
     private void OnCollisionEnter(Collision collision)
     {
         collision.gameObject.GetComponent<Rigidbody>().AddForce(Vector2.up * 500, ForceMode.Impulse);
